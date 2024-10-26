e424098b25dd596c7ff8bcdf5bb179c636171575
Nisa Fatima <nisafatima0000@gmail.com>
2024-10-26 19:05:32 +0500

    Added character

---
+++ Plan My Day/View/FrequentTaskButton.swift
@@ -0,0 +1,20 @@
+//
+//  FrequentTaskButton.swift
+//  Plan My Day
+//
+//  Created by Nisa on 26/10/2024.
+//
+
+import UIKit
+
+class FrequentTaskButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
